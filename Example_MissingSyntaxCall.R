b63e072e29eceb833db5add52d055cb55ab79ce1
Steven Brown <12702796+sbrown-iu@users.noreply.github.com>
2022-12-12 13:19:57 -0500

    Delete Example_MissingSyntaxCall.R

--- Example_MissingSyntaxCall.R
+++
@@ -1,8 +0,0 @@
-install.packages("I:/Projects/InfoEng/REDCap/MissingSyntax/REDCapMissingReporteR_0.1.0.tar.gz", source=TRUE, repos=NULL)
-library(REDCapMissingReporteR)
-
-dictionary <- "I:/Projects/InfoEng/REDCap/MissingSyntax/Package Development/IeDEAAYANINetwork_DataDictionary_2022-10-05.csv"
-out <- "I:/Projects/InfoEng/REDCap/MissingSyntax/Package Development/"
-forms <- c()
-types <- c("calc", "descriptive", "file")
-misscounter(filepath=dictionary, outpath=out, deleteforms=forms, deletetypes = types)
